e15fc56c10e8642dfe9ba12026c0b5046f711b85
Ömer Yasir Önal <engomeryasironal@gmail.com>
2024-02-27 12:30:46 +0300

    IleriSeviyeSwift project created

---
+++ IleriSeviyeSwift/IleriSeviyeSwift/UserClass.swift
@@ -0,0 +1,20 @@
+//
+//  UserClass.swift
+//  IleriSeviyeSwift
+//
+//  Created by Ömer Yasir Önal on 27.02.2024.
+//
+
+import Foundation
+
+class UserClass {
+    var isim : String
+    var yas : Int
+    var meslek : String
+    
+    init(isim: String, yas: Int, meslek: String) {
+        self.isim = isim
+        self.yas = yas
+        self.meslek = meslek
+    }
+}
---
+++ IleriSeviyeSwift/IleriSeviyeSwift/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  IleriSeviyeSwift
+//
+//  Created by Ömer Yasir Önal on 27.02.2024.
+//
+
+import Foundation
+
+let murat = UserClass(isim: "murat", yas: 20, meslek: "ogrenci")
+
+
+let muratStruct = UserStruct(isim: "murat", yas: 20, meslek: "ogrenci")
+print(murat.isim)
+print(muratStruct.isim)
